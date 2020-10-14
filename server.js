c7bf8e64172b979178f323c2e6771b72e39c01fc
loc <1st.loclau@example.com>
2020-10-14 09:31:23 +0700

    dm

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express")
+var ip = require('ip');
 const path = require("path")
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+app.use('/public',express.static(path.join(__dirname,'public')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'home.html'));
 });
 http.listen(process.env.PORT||3000, () => {});
-app.use('/public',express.static(path.join(__dirname,'public')))
+app.get('/api/api/api',(req,res)=>{
+    res.json(ip.address()+':'+process.env.PORT)
+})
 io.on('connection', (socket) => {
     io.emit('connected', {"msg":"connected"});
     socket.on('vitri',msg=>{
