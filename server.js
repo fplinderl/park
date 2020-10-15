e6366da4b2d1245534b7ecb2daecaa3465531e6c
loc <1st.loclau@example.com>
2020-10-15 23:18:24 +0700

    cors

--- server.js
+++ server.js
@@ -7,21 +7,20 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const upload = multer({ dest: "public" });
-app.all('/', function(req, res, next) {
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+app.get('/api/getall', control.getall)
+app.post("/api/uploadimage", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
-   });
-app.use('/public',express.static(path.join(__dirname,'public')))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'home.html'));
-});
-app.get('/api/getall',control.getall)
-app.post("/api/uploadimage", upload.single("file"), control.image);
+}, upload.single("file"), control.image);
 io.on('connection', (socket) => {
-    io.emit('connected', {"msg":"connected"});
-    socket.on('vitri',msg=>{
+    io.emit('connected', { "msg": "connected" });
+    socket.on('vitri', msg => {
         socket.broadcast.emit('vitriread', msg);
     })
 });
-http.listen(process.env.PORT||3001, () => {});
+http.listen(process.env.PORT || 3001, () => { });
