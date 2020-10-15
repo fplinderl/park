598d77d5b6c17a3a68d0b718e7095645052293db
loc <1st.loclau@example.com>
2020-10-15 23:55:41 +0700

    dcmdcm

--- control.js
+++ control.js
@@ -2,12 +2,7 @@ const CarModel = require("./model");
 const path = require("path")
 const fs = require("fs")
 var image = function (req, res, next) {
-  var oldname = path.join(__dirname, "./public/" + req.file.filename);
-  var newname = path.join(__dirname, "./public/" + req.file.originalname);
-  fs.rename(oldname, newname, (err) => {
-    if (err) console.log(err);
-  });
-  res.json("/public/" + req.file.originalname);
+  res.json(req.file.filename);
 }
 var getall = function(req,res){
   CarModel.find({})
@@ -18,4 +13,14 @@ var getall = function(req,res){
     res.status(500).json("loi getall roi")
   })
 }
-module.exports = {image,getall};
+var rename=function(req,res){
+  oldname=req.body.oldname
+  newname=req.body.newname
+  var oldname = path.join(__dirname, "./public/" + oldname);
+  var newname = path.join(__dirname, "./public/" + newname);
+  fs.rename(oldname, newname, (err) => {
+    if (err) console.log(err);
+  });
+  res.json(req.file);
+}
+module.exports = {image,getall,rename};
--- server.js
+++ server.js
@@ -3,16 +3,23 @@ var ip = require('ip');
 const path = require("path")
 const control = require("./control");
 const multer = require("multer");
+const bodyParser = require("body-parser");
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const upload = multer({ dest: "public" });
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use('/public', express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 app.get('/api/getall', control.getall)
-app.post("/api/uploadimage", function (req, res, next) {
+app.post('/api/rename', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+}, urlencodedParser, control.rename)
+app.post("/api/uploadimage", urlencodedParser, function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
