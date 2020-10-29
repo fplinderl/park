06a9f601727d411c489947d6374f2ca66ce6616d
loc <1st.loclau@example.com>
2020-10-29 17:19:04 +0700

    model

--- model.js
+++ model.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
-// mongoose.connect(
-//   'mongodb+srv://thanhloc:thanhlochp99@cluster0.gossl.mongodb.net/park?retryWrites=true&w=majority',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// )
-mongoose.connect('mongodb://localhost/park', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose.connect(
+  'mongodb+srv://thanhloc:thanhlochp99@cluster0.gossl.mongodb.net/park?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+// mongoose.connect('mongodb://localhost/park', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// });
 const Schema = mongoose.Schema;
 const carschema = new Schema(
   {
